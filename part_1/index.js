aa986aa2883c96a4f5dfc2239eca7771a0e0e4df
Sean Boyle <sean94@gmail.com>
2018-11-29 12:00:35 +0000

    Preload image into the canvas

---
+++ part_1/index.js
@@ -0,0 +1,26 @@
+'use strict'
+//sets up the variables for the app
+var img;
+
+//in order to use the img object  it has to be preload into the app
+function preload() {
+  img = loadImage('base_img.jpg')
+}
+
+function setup() {
+  createCanvas(800, 800);
+  noStroke();
+  //logs the whole object to the console
+  console.log(img);
+
+}
+
+function draw() {
+  // console.log(i);
+}
+
+//setsup the inputs for the application
+function keyReleased() {
+  //saves the canvas as a png
+  if (key == "s" || key == "S") saveCanvas(gd.timestamp(), "png");
+}
